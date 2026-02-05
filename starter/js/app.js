389ee25575996c449ce33bc52ba758e1ea44f5ad
Ayden Shine <105959128+ukennyo@users.noreply.github.com>
2026-02-05 01:42:23 -0500

    `Added functionality to toggle hamburger menu on click`

--- starter/js/app.js
+++ starter/js/app.js
@@ -1,5 +1,8 @@
 // JavaScript Brain of the site.
 // Responsibilites include: Filtering recipies, handling search, tags, adding interactivity (clicks, toggles etc.)
+
+// Finds an element using a CSS selector to add it in our file to toggle hamburger icon; warns me if I typed the class name wrong
+
 const getElement = (selector) =>{
     const element =document.querySelector(selector);
     if (element) return element
@@ -7,3 +10,11 @@ const getElement = (selector) =>{
         `Please dounle check your class name as there is no ${selector} class`)
        
     }
+    const links = getElement('.nav-links')
+    //navBtnDOM stores the button element in our HTML File for nav-btn
+    const navBtnDOM = getElement('.nav-btn')
+
+    //this part toggles the hamburger to add/remove the links
+    navBtnDOM.addEventListener('click', () => {
+        links.classList.toggle('show-links')
+    })
