e6aa3cba9bf457acd5bec17378a5af1b0e99b40e
Ayden Shine <105959128+ukennyo@users.noreply.github.com>
2026-02-05 01:20:35 -0500

    `Added getElement function to app.js`

--- starter/js/app.js
+++ starter/js/app.js
@@ -1,3 +1,9 @@
 // JavaScript Brain of the site.
 // Responsibilites include: Filtering recipies, handling search, tags, adding interactivity (clicks, toggles etc.)
-console.log('hello world')
+const getElement = (selector) =>{
+    const element =document.querySelector(selector);
+    if (element) return element
+    throw Error(
+        `Please dounle check your class name as there is no ${selector} class`)
+       
+    }
